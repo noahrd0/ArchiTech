59fd25eb3b0714eb1a3255be545b18605df882f8
Noah Radal <no.radal1203@gmail.com>
2024-04-06 16:52:06 +0200

    Adding password regex

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,6 +21,10 @@ exports.register = async (req, res) => {
         if (user) {
             return res.status(400).json({ message: 'Cette adresse email est déjà utilisée.' });
         }
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        if (!passwordRegex.test(password)) {
+            return res.status(400).json({ message: 'Le mot de passe doit contenir au moins une minuscule, une majuscule, un chiffre, un caractère spécial et avoir une longueur minimale de 8 caractères.' });
+        }
         const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash(password, salt);
         await User.create({ email: email, password: hash });
