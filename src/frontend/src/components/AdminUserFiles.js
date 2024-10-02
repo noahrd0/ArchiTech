c6d4fcf26e4cdb1a1cf2e5fa6483bf311d46c7c8
Veudzveulay <lucas.develay.ld@gmail.com>
2024-10-02 16:25:11 +0200

    Design view admin

--- src/frontend/src/components/AdminUserFiles.js
+++ src/frontend/src/components/AdminUserFiles.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilePdf, faFile, faTrashAlt } from '@fortawesome/free-solid-svg-icons'; // Importer l'icône de suppression
 import { useParams } from 'react-router-dom';
 
 const UserFileList = () => {
-    const { userId } = useParams(); // Récupérer l'ID de l'utilisateur depuis l'URL
+    const { userId } = useParams();
     const [files, setFiles] = useState([]);
     const [files_details, setFiles_details] = useState([]);
 
@@ -42,18 +44,26 @@ const UserFileList = () => {
     }, [userId]);
 
     return (
-        <div>
+        <div className='file-list-container'>
             <h2>Fichiers de l'utilisateur {userId}</h2>
             {files.length === 0 ? (
                 <p>Cet utilisateur n'a aucun fichier téléversé.</p>
             ) : (
-                <div className="file-list">
-                    {files.map((file, index) => (
-                        <div key={file.uuid} className="file-item">
-                            <img src={files_details[index]?.url} alt={file.name} width={100} height={100} style={{'object-fit': 'cover'}}/>
+                <div className='container-files'>
+                    <div className="file-list">
+                        {files.map((file, index) => (
+                            <div key={file.uuid} className="file-item">
+                            {file.name.endsWith('.pdf') ? (
+                                <FontAwesomeIcon icon={faFilePdf} size="3x" />
+                            ) : file.name.match(/\.(jpg|jpeg|png|gif)$/i) ? (
+                                <img src={files_details[index]?.url} alt={file.name} width={100} height={100} style={{'object-fit': 'cover'}}/>
+                            ) : (
+                                <FontAwesomeIcon icon={faFile} size="3x" />
+                            )}
                             <p>{file.name}</p>
                         </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
             )}
         </div>
